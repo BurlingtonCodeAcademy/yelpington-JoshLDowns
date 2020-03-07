bb0a5b9bf81a119229822f59e304fd1454ad1c67
JoshLDowns <53030592+JoshLDowns@users.noreply.github.com>
2020-03-07 14:55:16 -0500

    started working on restaurant page, worked on mobile styling

--- public/rest-page.js
+++ public/rest-page.js
@@ -1,21 +1,47 @@
-let id = window.location.href.slice(window.location.href.lastIndexOf('/')+1);
+let id = window.location.href.slice(window.location.href.lastIndexOf('/') + 1);
 
+let basicInfo = document.getElementById('rest-basic');
+let restDetail = document.getElementById('rest-detail');
+let restComments = document.getElementById('rest-comments');
+let home = document.getElementById('home');
 
-function buildMap() {
-    fetch(`resources/api/${id}`).then((data)=>{
+home.addEventListener('click', ()=> {
+    window.location='/index.html';
+});
+
+function buildRestData() {
+    fetch(`../resources/api/${id}.json`).then((data) => {
         return data.json()
-    }).then((object)=>{
-        console.log(object);
-    })
+    }).then((object) => {
+        return object;
+    }).then((singleRest) => {
+        console.log(singleRest);
+        console.log(JSON.parse(singleRest.coords));
+        let myMap = L.map('rest-map').setView(JSON.parse(singleRest.coords), 14.5);
 
-    let myMap = L.map('map').setView([44.478166, -73.214242], 14.5);
-    
-    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-        maxZoom: 18,
-        id: 'mapbox/dark-v10',
-        tileSize: 512,
-        zoomOffset: -1,
-        accessToken: 'pk.eyJ1Ijoiam9zaGxkb3ducyIsImEiOiJjazdkcDc0YXkwMGhtM2xwandocjRnb3piIn0.z7T6hs0PGj-jhNBGqV6yDQ'
-    }).addTo(myMap);
+        L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+            attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+            maxZoom: 18,
+            id: 'mapbox/dark-v10',
+            tileSize: 512,
+            zoomOffset: -1,
+            accessToken: 'pk.eyJ1Ijoiam9zaGxkb3ducyIsImEiOiJjazdkcDc0YXkwMGhtM2xwandocjRnb3piIn0.z7T6hs0PGj-jhNBGqV6yDQ'
+        }).addTo(myMap);
+
+        let marker = L.marker(JSON.parse(singleRest.coords)).addTo(myMap);
+        marker.bindPopup(`<p class = "popup-text" style = "margin: .5px; text-align: center; background-color: white;">${singleRest.name}</br>${singleRest.address}</p>`);
+
+        let name = singleRest.name;
+        let address = singleRest.address;
+        let phone = singleRest.phone;
+        let website = singleRest.website;
+        let hours = singleRest.hours;
+        let notes = singleRest.notes;
+
+        basicInfo.innerHTML+=`<h3>${name}</h3>`;
+        basicInfo.innerHTML+=`<p class='rest-info'>Phone: ${phone}</p>`;
+        basicInfo.innerHTML+=`<p class='rest-info'>Address: ${address}</p>`;
+        basicInfo.innerHTML+=`<p class='rest-info'>Hours: ${hours}</p>`;
+        basicInfo.innerHTML+=`<a href='${website}' target='_blank' class='rest-info'>Website</a>`;
+    })
 }
--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const fs = require('fs');
-const fetch = require('node-fetch');
 const port = process.env.PORT || 5000;
 
 app.use(express.static('public'));
@@ -11,18 +9,5 @@ app.get('/rest-page/:id', (req, res) => {
     res.sendFile(path.resolve(`public/rest-page.html`));
 })
 
-function fillAPI () {
-    fetch('public/resources/api/rest-list.json').then((data)=>{
-        return data.json();
-    }).then((jsonObj)=>{
-        jsonObj.forEach((rest)=>{
-            fs.writeFile(`public/resources/api/${rest.id}`, jsonObj, ()=>{
-                console.log(`Write Successful!`)
-            })
-        })
-    })
-}
-
-//fillAPI();
 
 app.listen(port, ()=> console.log(`Example app listening on port: ${port}`))
